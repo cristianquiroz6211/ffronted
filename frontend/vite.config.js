2f91bc1c046b77262c06595ad0ca02cd0ef15de3
cristianquiroz6211 <141106618+cristianquiroz6211@users.noreply.github.com>
2024-11-08 23:16:25 -0500

    certificate

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: {
+      key: fs.readFileSync('C:/Users/Cristian/Downloads/ffronted/ffronted/frontend/certs/localhost-key.pem'), // Ruta a tu clave privada
+      cert: fs.readFileSync('C:/Users/Cristian/Downloads/ffronted/ffronted/frontend/certs/localhost.pem') // Ruta a tu certificado
+    },
     port: 3000,
     proxy: {
       '/general/api/v1': {
